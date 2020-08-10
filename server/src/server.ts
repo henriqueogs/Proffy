006b6b2163fda0030f9f2f303d9a1eb9bf936eef
Henrique <henriqueogs@gmail.com>
2020-08-10 16:25:02 -0300

    "creat migrations"

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,2 +1,12 @@
 import express from 'express';
 
+const app = express();
+
+app.use(express.json);
+
+
+app.get('/' , (request, response) => {
+    return response.json({message: "Hello"})
+})
+
+app.listen(3333)
