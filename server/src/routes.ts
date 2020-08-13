6124d4a5b57a80667c7ca832481f1ae2d15b816f
Henrique <henriqueogs@gmail.com>
2020-08-13 00:12:39 -0300

    Add diretorios

---
+++ server/src/routes.ts
@@ -0,0 +1,50 @@
+import express from 'express';
+import db from './database/connection';
+
+const routes = express.Router();
+
+interface ScheduleItem {
+    week_day: number,
+    from: string,
+    to: string
+}
+
+routes.post('/classes' , async(request, response) => {
+    const {
+        name,
+        avatar,
+        whatsapp,
+        bio,
+        subject,
+        cost,
+        schedule
+    } = request.body;
+
+    const insertedUsersIds = await db('users').insert({
+        name,
+        avatar,
+        whatsapp,
+        bio,
+    });
+
+    const user_id = insertedUsersIds[0];
+
+    const insertedClassesIds = await db('classes').insert({
+        subject,
+        cost,
+        user_id,
+    });
+
+    const classe_id = insertedClassesIds[0];
+
+    const classSchedule = schedule.map((scheduleItem: any) => {
+        return{
+
+        }
+    });
+
+
+    return response.send();
+});
+
+export default routes;
